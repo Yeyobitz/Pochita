6cb039ced92d433279bc7a7b5cfbf8e831fe7863
Yeyobitz <diegoqm96@hotmail.com>
2024-11-18 00:05:42 -0300

    Add fields for category, description, image, and price to InventoryItem model

--- inventory/models.py
+++ inventory/models.py
@@ -5,8 +5,13 @@ class InventoryItem(models.Model):
     stock = models.PositiveIntegerField()
     supplier = models.CharField(max_length=100)
     expiration_date = models.DateField()
+    image = models.ImageField(upload_to='inventory_images/', null=True, blank=True)
+    description = models.TextField(default='')
+    category = models.CharField(max_length=50, default='General')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
--- inventory/views.py
+++ inventory/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import InventoryItem
 from django.urls import reverse
 
-# Listar productos
 def list_inventory(request):
     inventory = InventoryItem.objects.all().order_by('name')
     return render(request, 'inventory/list_inventory.html', {'inventory': inventory})
 
-# Agregar o actualizar productos
 def update_inventory(request, pk=None):
     item = None
     if pk:
@@ -18,15 +16,33 @@ def update_inventory(request, pk=None):
         stock = request.POST['stock']
         supplier = request.POST['supplier']
         expiration_date = request.POST['expiration_date']
+        image = request.FILES.get('image')
+        description = request.POST['description']
+        category = request.POST['category']
+        price = request.POST['price']
 
         if item:
             item.name = name
             item.stock = stock
             item.supplier = supplier
             item.expiration_date = expiration_date
+            item.description = description
+            item.category = category
+            item.price = price
+            if image:
+                item.image = image
             item.save()
         else:
-            InventoryItem.objects.create(name=name, stock=stock, supplier=supplier, expiration_date=expiration_date)
+            InventoryItem.objects.create(
+                name=name, 
+                stock=stock, 
+                supplier=supplier, 
+                expiration_date=expiration_date,
+                image=image,
+                description=description,
+                category=category,
+                price=price
+            )
         return redirect(reverse('list_inventory'))
 
     return render(request, 'inventory/update_inventory.html', {'item': item})
