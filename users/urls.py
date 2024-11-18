595c95cdad638c7c572225fba9565f4c59d75de8
Rayoid <rayoidpls@gmail.com>
2024-11-18 06:58:34 -0300

    Multi pet register+profile for clients+test user

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path,  include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('register/', views.register_client, name='register_client'),
+    path('profile/', views.profile_view, name='profile'),
 ]
